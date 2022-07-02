3cb90dac0010bca23c107b54c98bc48448f7cd86
davidpengiupui <90153025+davidpengiupui@users.noreply.github.com>
2022-07-02 16:57:45 -0400

    Update loss.py

--- loss.py
+++ loss.py
@@ -92,3 +92,31 @@ class JointsKLLoss(nn.Module):
             return loss.mean()
         elif self.reduction == 'none':
             return loss.mean(dim=-1)
+        
+def weighted_neg_loss(pred, gt, weight):
+    """Modified focal loss. Exactly the same as CornerNet.
+      Runs faster and costs a little bit more memory
+    Arguments:
+      pred (batch x c x h x w)
+      gt_regr (batch x c x h x w)
+    """
+    pos_inds = gt.eq(1).float()
+    neg_inds = gt.lt(1).float() * weight.lt(1).float()
+
+    neg_weights = torch.pow(1 - gt, 4)
+
+    loss = 0
+
+    pos_loss = torch.log(pred) * torch.pow(1 - pred, 2) * pos_inds
+    neg_loss = torch.log(1 - pred) * torch.pow(pred, 2) * neg_weights * \
+        neg_inds
+
+    num_pos = pos_inds.float().sum()
+    pos_loss = pos_loss.sum()
+    neg_loss = neg_loss.sum()
+
+    if num_pos == 0:
+        loss = loss - neg_loss
+    else:
+        loss = loss - (pos_loss + neg_loss) / num_pos
+    return loss
